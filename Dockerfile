c5dc3e9066c867bf38a1c1a478efd7ff3556edf1
shimwell <mail@jshimwell.com>
2019-05-26 20:43:18 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,86 @@
+# Use the standard Nginx image from Docker Hub which is based on debian:stretch-slim
+FROM nginx
+
+# build with 
+# sudo docker build -t shimwell/nginx .
+# run with
+# sudo docker run -p 8080:8080 -u 143 shimwell/nginx
+# navigate to http://127.0.0.1:8080/
+
+ENV HOME=/opt/repo
+
+# install python, uwsgi, and supervisord
+RUN apt-get update && apt-get install -y supervisor uwsgi python3 python3-pip procps vim && \
+    /usr/bin/pip3 install uwsgi==2.0.17 flask==1.0.2
+
+RUN pip3 install pymongo 
+
+
+
+# Source code file
+COPY src ${HOME}/src
+RUN echo 'copying updated src'
+
+# Copy the configuration file from the current directory and paste 
+# it inside the container to use it as Nginx's default config.
+COPY deployment/nginx.conf /etc/nginx/nginx.conf
+
+# setup NGINX config
+RUN mkdir -p /spool/nginx /run/pid && \
+    chmod -R 777 /var/log/nginx /var/cache/nginx /etc/nginx /var/run /run /run/pid /spool/nginx && \
+    chgrp -R 0 /var/log/nginx /var/cache/nginx /etc/nginx /var/run /run /run/pid /spool/nginx && \
+    chmod -R g+rwX /var/log/nginx /var/cache/nginx /etc/nginx /var/run /run /run/pid /spool/nginx && \
+    rm /etc/nginx/conf.d/default.conf
+
+# Copy the base uWSGI ini file to enable default dynamic uwsgi process number
+# RUN echo recopy
+COPY deployment/uwsgi.ini /etc/uwsgi/apps-available/uwsgi.ini
+RUN ln -s /etc/uwsgi/apps-available/uwsgi.ini /etc/uwsgi/apps-enabled/uwsgi.ini
+
+COPY deployment/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+RUN touch /var/log/supervisor/supervisord.log
+
+EXPOSE 8080:8080
+
+# setup entrypoint
+COPY deployment/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+
+# access to /dev/stdout
+# https://github.com/moby/moby/issues/31243#issuecomment-406879017
+RUN ln -s /usr/local/bin/docker-entrypoint.sh / && \
+    chmod 777 /usr/local/bin/docker-entrypoint.sh && \
+    chgrp -R 0 /usr/local/bin/docker-entrypoint.sh && \
+    chown -R nginx:root /usr/local/bin/docker-entrypoint.sh
+
+# https://docs.openshift.com/container-platform/3.3/creating_images/guidelines.html
+RUN chgrp -R 0 /var/log /var/cache /run/pid /spool/nginx /var/run /run /tmp /etc/uwsgi /etc/nginx && \
+    chmod -R g+rwX /var/log /var/cache /run/pid /spool/nginx /var/run /run /tmp /etc/uwsgi /etc/nginx && \
+    chown -R nginx:root ${HOME} && \
+    chmod -R 777 ${HOME} /etc/passwd
+
+# from flask import Flask
+# from flask import Flask, jsonify, render_template, request, Response, send_file
+
+
+
+# import requests
+# import json
+# from bson import json_util
+# from bson.objectid import ObjectId
+# import requests
+# import re
+# from flask import Flask
+# from flask import Flask, jsonify, render_template, request, Response
+# from flask_cors import CORS, cross_origin
+# import os
+# from data_formatting_tools import *
+# from database_tools import *
+# from io import BytesIO
+# from io import StringIO
+
+
+
+# enter
+WORKDIR ${HOME}
+ENTRYPOINT ["docker-entrypoint.sh"]
+CMD ["supervisord"]
---
+++ src/wsgi.py
@@ -0,0 +1,350 @@
+import math
+import time
+from flask import Flask, jsonify, make_response, abort, request
+
+import json
+from bson import json_util
+from bson.objectid import ObjectId
+from data_formatting_tools import *
+from database_tools import *
+
+app = Flask(__name__)
+
+VERSION = '0.1'
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+# dummy API
+@app.route('/api/v{}/theanswer'.format(VERSION), methods=['GET'])
+def theanswer():
+    return jsonify({ 'answer': 42 })
+
+# add API
+@app.route('/api/v{}/add'.format(VERSION), methods=['POST'])
+def add():
+    if not request.json or not 'data' in request.json:
+        abort(400)
+
+    jsonraw = request.json
+    result = -1.0
+    if 'data' in jsonraw and len(jsonraw['data']) > 0:
+        result = sum(jsonraw['data'])
+
+    return jsonify({ 'result': result })
+
+# silly work API
+@app.route('/api/v{}/dowork'.format(VERSION), methods=['POST'])
+def dowork():
+    if not request.json or not 'data' in request.json:
+        abort(400)
+
+    jsonraw = request.json
+    result = -1.0
+    if 'data' in jsonraw and len(jsonraw['data']) > 0:
+        result = 2.4
+        for d in jsonraw['data']:
+            result += math.pow(float(d)*4.51e2/5.123e2, 2.3)
+
+    time.sleep(3)
+    return jsonify({ 'result': result })
+
+@app.route('/')
+def index():
+    return "Test rest API v{0}".format(VERSION)
+
+
+
+
+
+
+
+# from flask import Flask
+# from flask import Flask, jsonify, render_template, request, Response, send_file
+
+
+# import json
+# import pandas as pd
+
+# import requests
+# import json
+# import requests
+# import re
+# from flask import Flask
+# from flask import Flask, jsonify, render_template, request, Response
+# from flask_cors import CORS, cross_origin
+# import os
+# from io import BytesIO
+# from io import StringIO
+
+# # os.system('mongod --bind_ip_all &')
+
+# #collection, client, db = connect_to_database()
+# collection, client, db = connect_to_docker_database()
+
+# all_database_fields = get_database_fields(collection)
+
+
+# # meta_data_fields = get_database_fields(collection, ignore_fields=['Time [sec]', 'Stroke', 'Extens', 'Load', 'Temp1', 'Temp2', 'Temp3'])
+# # print(meta_data_fields)
+
+# meta_data_fields = find_all_fields_not_of_a_particular_types_in_database(collection,'list')
+# print('meta_data_fields',meta_data_fields)
+
+# axis_option_fields = find_all_fields_of_a_particular_types_in_database(collection,'list')
+# print('axis_option_fields',axis_option_fields)
+
+# # metadata_values=[]
+# metadata_fields_and_their_distinct_values={}
+# for entry in meta_data_fields:
+#     values = get_entries_in_field(collection,entry)
+#     # metadata_values.append(values)
+#     metadata_fields_and_their_distinct_values[entry]=values
+
+
+# meta_data_fields_and_distinct_entries = []
+# for field in meta_data_fields:
+#     meta_data_fields_and_distinct_entries.append({'field':[field],'distinct_values':metadata_fields_and_their_distinct_values[field]})
+
+# #print(meta_data_fields_and_distinct_entries)
+# #print(find_metadata_fields_and_their_distinct_values(collection, ignore_fields=['Time [sec]', 'Stroke', 'Extens', 'Load', 'Temp1', 'Temp2', 'Temp3']))
+
+# # print('all_database_fields',all_database_fields)
+# # print('meta_data_fields',meta_data_fields)
+# # print('meta_data_fields',metadata_values)
+
+
+# template_dir = os.path.abspath('../build')
+# static_dir= os.path.abspath("../build/static")
+# print('static_dir',static_dir)
+# app = Flask(__name__, template_folder=template_dir, static_folder=static_dir )
+
+# CORS(app)
+
+
+# def convert_query_string_to_query_dict(query_string):
+#     try:
+#         query ={}
+#         for x in query_string.strip('{}').split(','):
+#             entry=x.split(':')
+#             query[entry[0]]=entry[1]
+#     except:
+#         query={}
+#     return query
+
+
+# def get_entries_in_field(collection, field, query=None):
+#     if query != {}:
+#       result = collection.distinct(field,query)
+#     else:
+#       result = collection.distinct(field)
+#     return result
+
+
+
+# @app.route('/download_py3' ,methods=['GET','POST'])
+# @cross_origin()
+# def download4():
+#     ids = request.args.get('ids')
+#     ids = ids.strip("'")
+#     ids = ids.strip('"')
+#     ids = ids.strip("[")
+#     ids = ids.strip("]")
+#     ids = ids.split(',')
+#     list_of_matching_database_entries = []
+#     print('ids', ids  )
+#     for id in ids:
+#         id = id.strip("'")
+#         id = id.strip('"')             
+#         print('id', id)
+#         query={'_id':ObjectId(id)}
+#         result = collection.find_one(query)
+#         results_json = json_util.dumps(result)
+#         list_of_matching_database_entries.append(results_json)
+
+#     file_data = BytesIO()
+#     # file_data = StringIO()
+#     file_data.write(str(list_of_matching_database_entries).encode('utf-8'))
+#     #file_data.write(b'list_of_matching_database_entries')
+#     file_data.seek(0)
+#     print('making file')
+#     return send_file(file_data, attachment_filename='my_data.txt', as_attachment=True)
+
+
+
+# @app.route('/find_distinct_entries' ,methods=['GET','POST'])
+# @cross_origin()
+# def find_distinct_entries():
+#     field = request.args.get('field')
+#     print('field',field)
+#     distinct_entries = metadata_fields_and_their_distinct_values[field]
+#     print(distinct_entries)
+#     return jsonify(distinct_entries)
+
+
+# @app.route('/find_distinct_entries_in_a_field' ,methods=['GET','POST'])
+# @cross_origin()
+# def find_distinct_entries_in_a_field():
+#     field = request.args.get('field')
+#     query_string = request.args.get('query')
+
+#     query = convert_query_string_to_query_dict(query_string)
+
+#     result = get_entries_in_field(collection, field, query)
+
+#     return jsonify(result)
+
+
+
+# @app.route('/find_meta_data_fields' ,methods=['GET','POST'])
+# @cross_origin()
+# def find_meta_data_fields():
+#     return jsonify(meta_data_fields)
+
+# @app.route('/find_axis_data_fields' ,methods=['GET','POST'])
+# @cross_origin()
+# def find_axis_data_fields():
+#     return jsonify(axis_option_fields)
+
+
+# @app.route('/find_meta_data_fields_and_distinct_entries' ,methods=['GET','POST'])
+# @cross_origin()
+# def find_meta_data_fields_and_distinct_entries():
+#     return jsonify(meta_data_fields_and_distinct_entries)
+
+
+
+
+# @app.route('/get_number_of_matching_entrys' ,methods=['GET','POST'])
+# @cross_origin()
+# def get_number_of_matching_entrys():
+#     query_string = request.args.get('query')
+
+#     query = convert_query_string_to_query_dict(query_string)
+
+#     results = collection.find(query)
+#     counter=0
+#     for document in results:
+#         counter=counter+1
+#     return jsonify(counter)
+
+
+# @app.route('/get_matching_entrys' ,methods=['GET','POST'])
+# @cross_origin()
+# def get_matching_entrys():
+#     query_string = request.args.get('query')
+#     limit = request.args.get('limit', default=100, type=int)
+
+#     print('query_string',query_string)
+#     try:
+#         query ={}
+#         query_string= query_string.replace('"', '').replace("'", '')
+#         for x in query_string.strip('{}').split(','):
+#             entry=x.split(':')
+#             query[entry[0]]=entry[1]
+#     except:
+#         query={}
+#     print('query = ',query)
+#     result = collection.find(query).limit(limit)
+#     results_json = json_util.dumps(result)
+#     return results_json
+
+# @app.route('/get_matching_entrys_limited_fields' ,methods=['GET','POST'])
+# @cross_origin()
+# def get_matching_entrys_limited_fields():
+#     query_string = request.args.get('query')
+#     requested_fields_string = request.args.get('fields')
+#     limit = request.args.get('limit', default=100, type=int)
+
+
+
+#     print('query_string',query_string)
+#     print('fields_string',requested_fields_string)
+
+#     try:
+#         query ={}
+#         query_string= query_string.replace('"', '').replace("'", '')
+#         for x in query_string.strip('{}').split(','):
+#             entry=x.split(':')
+#             query[entry[0]]=entry[1]
+#     except:
+#         query={}
+
+#     try:
+#         fields ={}
+#         requested_fields_string= requested_fields_string.replace('"', '').replace("'", '')
+#         for x in requested_fields_string.strip('{}').split(','):
+#             entry=x.split(':')
+#             fields[entry[0]]=int(entry[1])
+#     except:
+#         fields={}
+
+
+#     print('fields = ',fields)
+#     print('query = ',query)
+
+#     result = collection.find(query,fields).limit(limit)
+#     results_json = json_util.dumps(result)
+#     return results_json
+
+
+# @app.route('/get_matching_entry' ,methods=['GET','POST'])
+# @cross_origin()
+# def get_matching_entry():
+#     query_string = request.args.get('query')
+
+#     print('query_string',query_string)
+#     try:
+#         query ={}
+#         query_string= query_string.replace('"', '').replace("'", '')
+#         for x in query_string.strip('{}').split(','):
+#             entry=x.split(':')
+#             query[entry[0]]=entry[1]
+#     except:
+#         query={}
+#     print('query = ',query)
+#     result = collection.find_one(query)
+#     results_json = json_util.dumps(result)
+#     return results_json
+
+
+
+# @app.route('/test' ,methods=['GET','POST'])
+# @cross_origin()
+# def return_test():
+#     return 'test working'
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5001)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=False,
+        host='0.0.0.0',
+        port=8080
+    )
