42b0c54b22c779332b02a752717a7afb68257fde
shimwell <mail@jshimwell.com>
2019-06-02 23:38:21 +0100

    fixed query forming method to allow ints

--- src/wsgi.py
+++ src/wsgi.py
@@ -87,77 +87,77 @@ for field in meta_data_fields:
 
 
 
-# def convert_query_string_to_query_dict(query_string):
-#     try:
-#         query ={}
-#         for x in query_string.strip('{}').split(','):
-#             entry=x.split(':')
-#             query[entry[0]]=entry[1]
-#     except:
-#         query={}
-#     return query
+def convert_query_string_to_query_dict(query_string):
+    try:
+        query ={}
+        for x in query_string.strip('{}').split(','):
+            entry=x.split(':')
+            query[entry[0]]=entry[1]
+    except:
+        query={}
+    return query
 
 
-# def get_entries_in_field(collection, field, query=None):
-#     if query != {}:
-#       result = collection.distinct(field,query)
-#     else:
-#       result = collection.distinct(field)
-#     return result
-
-
-
-# @app.route('/download_py3' ,methods=['GET','POST'])
-# @cross_origin()
-# def download4():
-#     ids = request.args.get('ids')
-#     ids = ids.strip("'")
-#     ids = ids.strip('"')
-#     ids = ids.strip("[")
-#     ids = ids.strip("]")
-#     ids = ids.split(',')
-#     list_of_matching_database_entries = []
-#     print('ids', ids  )
-#     for id in ids:
-#         id = id.strip("'")
-#         id = id.strip('"')             
-#         print('id', id)
-#         query={'_id':ObjectId(id)}
-#         result = collection.find_one(query)
-#         results_json = json_util.dumps(result)
-#         list_of_matching_database_entries.append(results_json)
-
-#     file_data = BytesIO()
-#     # file_data = StringIO()
-#     file_data.write(str(list_of_matching_database_entries).encode('utf-8'))
-#     #file_data.write(b'list_of_matching_database_entries')
-#     file_data.seek(0)
-#     print('making file')
-#     return send_file(file_data, attachment_filename='my_data.txt', as_attachment=True)
+def get_entries_in_field(collection, field, query=None):
+    if query != {}:
+      result = collection.distinct(field,query)
+    else:
+      result = collection.distinct(field)
+    return result
 
 
 
-# @app.route('/find_distinct_entries' ,methods=['GET','POST'])
-# @cross_origin()
-# def find_distinct_entries():
-#     field = request.args.get('field')
-#     print('field',field)
-#     distinct_entries = metadata_fields_and_their_distinct_values[field]
-#     print(distinct_entries)
-#     return jsonify(distinct_entries)
+@app.route('/download_py3' ,methods=['GET','POST'])
+@cross_origin()
+def download4():
+    ids = request.args.get('ids')
+    ids = ids.strip("'")
+    ids = ids.strip('"')
+    ids = ids.strip("[")
+    ids = ids.strip("]")
+    ids = ids.split(',')
+    list_of_matching_database_entries = []
+    print('ids', ids  )
+    for id in ids:
+        id = id.strip("'")
+        id = id.strip('"')             
+        print('id', id)
+        query={'_id':ObjectId(id)}
+        result = collection.find_one(query)
+        results_json = json_util.dumps(result)
+        list_of_matching_database_entries.append(results_json)
+
+    file_data = BytesIO()
+    # file_data = StringIO()
+    file_data.write(str(list_of_matching_database_entries).encode('utf-8'))
+    #file_data.write(b'list_of_matching_database_entries')
+    file_data.seek(0)
+    print('making file')
+    return send_file(file_data, attachment_filename='my_data.txt', as_attachment=True)
 
 
-# @app.route('/find_distinct_entries_in_a_field' ,methods=['GET','POST'])
-# @cross_origin()
-# def find_distinct_entries_in_a_field():
-#     field = request.args.get('field')
-#     query_string = request.args.get('query')
 
-#     query = convert_query_string_to_query_dict(query_string)
+@app.route('/find_distinct_entries' ,methods=['GET','POST'])
+@cross_origin()
+def find_distinct_entries():
+    field = request.args.get('field')
+    print('field',field)
+    distinct_entries = metadata_fields_and_their_distinct_values[field]
+    print(distinct_entries)
+    return jsonify(distinct_entries)
 
-#     result = get_entries_in_field(collection, field, query)
 
-#     return jsonify(result)
+@app.route('/find_distinct_entries_in_a_field' ,methods=['GET','POST'])
+@cross_origin()
+def find_distinct_entries_in_a_field():
+    field = request.args.get('field')
+    query_string = request.args.get('query')
+
+    query = convert_query_string_to_query_dict(query_string)
+
+    result = get_entries_in_field(collection, field, query)
+
+    return jsonify(result)
 
 
 
@@ -180,97 +180,99 @@ def find_meta_data_fields_and_distinct_entries():
 
 
 
-# @app.route('/get_number_of_matching_entrys' ,methods=['GET','POST'])
-# @cross_origin()
-# def get_number_of_matching_entrys():
-#     query_string = request.args.get('query')
+@app.route('/get_number_of_matching_entrys' ,methods=['GET','POST'])
+@cross_origin()
+def get_number_of_matching_entrys():
+    query_string = request.args.get('query')
 
-#     query = convert_query_string_to_query_dict(query_string)
+    query = convert_query_string_to_query_dict(query_string)
 
-#     results = collection.find(query)
-#     counter=0
-#     for document in results:
-#         counter=counter+1
-#     return jsonify(counter)
-
-
-# @app.route('/get_matching_entrys' ,methods=['GET','POST'])
-# @cross_origin()
-# def get_matching_entrys():
-#     query_string = request.args.get('query')
-#     limit = request.args.get('limit', default=100, type=int)
-
-#     print('query_string',query_string)
-#     try:
-#         query ={}
-#         query_string= query_string.replace('"', '').replace("'", '')
-#         for x in query_string.strip('{}').split(','):
-#             entry=x.split(':')
-#             query[entry[0]]=entry[1]
-#     except:
-#         query={}
-#     print('query = ',query)
-#     result = collection.find(query).limit(limit)
-#     results_json = json_util.dumps(result)
-#     return results_json
-
-# @app.route('/get_matching_entrys_limited_fields' ,methods=['GET','POST'])
-# @cross_origin()
-# def get_matching_entrys_limited_fields():
-#     query_string = request.args.get('query')
-#     requested_fields_string = request.args.get('fields')
-#     limit = request.args.get('limit', default=100, type=int)
+    results = collection.find(query)
+    counter=0
+    for document in results:
+        counter=counter+1
+    return jsonify(counter)
 
 
+@app.route('/get_matching_entrys' ,methods=['GET','POST'])
+@cross_origin()
+def get_matching_entrys():
+    query_string = request.args.get('query')
+    limit = request.args.get('limit', default=50, type=int)
 
-#     print('query_string',query_string)
-#     print('fields_string',requested_fields_string)
+    # print('query_string',query_string)
+    # try:
+    #     query ={}
+    #     query_string= query_string.replace('"', '').replace("'", '')
+    #     for x in query_string.strip('{}').split(','):
+    #         entry=x.split(':')
+    #         query[entry[0]]=entry[1]
+    # except:
+    #     query={}
+    query = json.loads(query_string)
+    print('query = ',query)
+    result = collection.find(query).limit(limit)
+    results_json = json_util.dumps(result)
+    return results_json
 
-#     try:
-#         query ={}
-#         query_string= query_string.replace('"', '').replace("'", '')
-#         for x in query_string.strip('{}').split(','):
-#             entry=x.split(':')
-#             query[entry[0]]=entry[1]
-#     except:
-#         query={}
-
-#     try:
-#         fields ={}
-#         requested_fields_string= requested_fields_string.replace('"', '').replace("'", '')
-#         for x in requested_fields_string.strip('{}').split(','):
-#             entry=x.split(':')
-#             fields[entry[0]]=int(entry[1])
-#     except:
-#         fields={}
-
-
-#     print('fields = ',fields)
-#     print('query = ',query)
-
-#     result = collection.find(query,fields).limit(limit)
-#     results_json = json_util.dumps(result)
-#     return results_json
+@app.route('/get_matching_entrys_limited_fields' ,methods=['GET','POST'])
+@cross_origin()
+def get_matching_entrys_limited_fields():
+    query_string = request.args.get('query')
+    requested_fields_string = request.args.get('fields')
+    limit = request.args.get('limit', default=50, type=int)
 
 
-# @app.route('/get_matching_entry' ,methods=['GET','POST'])
-# @cross_origin()
-# def get_matching_entry():
-#     query_string = request.args.get('query')
 
-#     print('query_string',query_string)
-#     try:
-#         query ={}
-#         query_string= query_string.replace('"', '').replace("'", '')
-#         for x in query_string.strip('{}').split(','):
-#             entry=x.split(':')
-#             query[entry[0]]=entry[1]
-#     except:
-#         query={}
-#     print('query = ',query)
-#     result = collection.find_one(query)
-#     results_json = json_util.dumps(result)
-#     return results_json
+    print('query_string',query_string)
+    print('fields_string',requested_fields_string)
+
+    # try:
+    #     query ={}
+    #     query_string= query_string.replace('"', '').replace("'", '')
+    #     for x in query_string.strip('{}').split(','):
+    #         entry=x.split(':')
+    #         query[entry[0]]=entry[1]
+    # except:
+    #     query={}
+    query = json.loads(query_string)
+
+    # try:
+    #     fields ={}
+    #     requested_fields_string= requested_fields_string.replace('"', '').replace("'", '')
+    #     for x in requested_fields_string.strip('{}').split(','):
+    #         entry=x.split(':')
+    #         fields[entry[0]]=int(entry[1])
+    # except:
+    #     fields={}
+    fields = json.loads(requested_fields_string)
+
+    print('fields = ',fields)
+    print('query = ',query)
+
+    result = collection.find(query,fields).limit(limit)
+    results_json = json_util.dumps(result)
+    return results_json
+
+
+@app.route('/get_matching_entry' ,methods=['GET','POST'])
+@cross_origin()
+def get_matching_entry():
+    query_string = request.args.get('query')
+
+    print('query_string',query_string)
+    try:
+        query ={}
+        query_string= query_string.replace('"', '').replace("'", '')
+        for x in query_string.strip('{}').split(','):
+            entry=x.split(':')
+            query[entry[0]]=entry[1]
+    except:
+        query={}
+    print('query = ',query)
+    result = collection.find_one(query)
+    results_json = json_util.dumps(result)
+    return results_json
 
 
 
